202f56eee7d6157b106bbaa8cc5cb5acc0079e90
crmillsfsu <97139671+crmillsfsu@users.noreply.github.com>
2025-05-19 07:13:47 -0400

    initial commit

---
+++ Asana.CLI/Program.cs
@@ -0,0 +1,14 @@
+﻿using Asana.Library.Models;
+using System;
+
+namespace Asana
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ToDo MyFirstToDo = new ToDo();
+            Console.WriteLine(MyFirstToDo.Name?.Length);
+        }
+    }
+}
