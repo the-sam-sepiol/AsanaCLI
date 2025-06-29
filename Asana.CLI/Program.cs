f9c45abe82b3aefb57feaab062456b7b1a3e695b
samsep <ela22d@fsu.edu>
2025-06-29 14:28:28 -0400

    Refactor ToDo and Project management into service class
    - Moved ToDo and Project management methods from `Program.cs` to a new `ToDoServiceProxy.cs` for better organization and separation of concerns.
    - Updated `Program.cs` to utilize the new service class, modifying method calls to pass necessary parameters.
    - Changed `nextProjectId` from private static to static, affecting its accessibility.

--- Asana.CLI/Program.cs
+++ Asana.CLI/Program.cs
@@ -7,6 +7,7 @@ using System.Reflection.Metadata.Ecma335;
 using System.Runtime.CompilerServices;
 using System.Collections.Generic;
 using System.Linq;
+using Asana.Library.Services;
 
 namespace Asana
 {
@@ -15,7 +16,8 @@ namespace Asana
         private static List<Project> projects = new();
         private static List<ToDo> toDos = new();
         private static int nextToDoId = 1;
-        private static int nextProjectId = 1;
+        
+        static int nextProjectId = 1;
         public static void Main(string[] args)
         {
             int choiceInt;
@@ -43,37 +45,37 @@ namespace Asana
                         case 0:
                             break;
                         case 1:
-                            var todo = CreateToDo();
+                            var todo = ToDoServiceProxy.CreateToDo(ref nextToDoId);
                             toDos.Add(todo);
                             Console.WriteLine($"Created ToDo: {todo}");
                             break;
                         case 2:
-                            if (DeleteToDo() == true)
+                            if (ToDoServiceProxy.DeleteToDo(toDos) == true)
                                 Console.WriteLine("ToDo deleted.");
                             break;
                         case 3:
-                            UpdateTodo();
+                            ToDoServiceProxy.UpdateTodo(toDos);
                             break;
                         case 4:
-                            ListAllToDos();
+                            ToDoServiceProxy.ListAllToDos(toDos);
                             break;
                         case 5:
-                            var proj = CreateProject();
+                            var proj = ToDoServiceProxy.CreateProject(ref nextProjectId);
                             projects.Add(proj);
                             Console.WriteLine($"Created project: {proj}");
                             break;
                         case 6:
-                            if (DeleteProject() == true)
+                            if (ToDoServiceProxy.DeleteProject(projects) == true)
                                 Console.WriteLine("Project deleted.");
                             break;
                         case 7:
-                            UpdateProject();
+                            ToDoServiceProxy.UpdateProject(toDos, projects);
                             break;
                         case 8:
-                            ListAllProjects();
+                            ToDoServiceProxy.ListAllProjects(projects);
                             break;
                         case 9:
-                            ListGivenProjectToDo();
+                            ToDoServiceProxy.ListGivenProjectToDo(projects);
                             break;
                         default:
                             Console.WriteLine("ERROR: Unknown menu selection.");
@@ -88,217 +90,7 @@ namespace Asana
             } while (choiceInt != 0);
 
         }
-        private static bool DeleteToDo()
-        {
-            Console.Write("Enter ToDo ID: ");
-            if (int.TryParse(Console.ReadLine(), out var id))
-            {
-                int index = -1;
-                for (int i = 0; i < toDos.Count; i++)
-                    if (toDos[i].Id == id)
-                    {
-                        index = i;
-                        break;
-                    }
-                if (index >= 0)
-                {
-                    toDos.RemoveAt(index);
-                    return true;
-                }
-                else
-                {
-                    Console.WriteLine("ToDo ID invalid.");
-                    return false;
-                }
-            }
-            else
-            {
-                Console.WriteLine("ToDo ID invalid.");
-                return false;
-            }
-        }
-        private static void UpdateTodo()
-        {
-            Console.Write("Enter ToDo ID: ");
-            if (int.TryParse(Console.ReadLine(), out var id))
-            {
-                int index = -1;
-                for (int i = 0; i < toDos.Count; i++)
-                    if (toDos[i].Id == id)
-                    {
-                        index = i;
-                        break;
-                    }
-                if (index >= 0)
-                {
-                    Console.Write("Name: ");
-                    toDos[index].Name = Console.ReadLine();
-                    Console.Write("Description: ");
-                    toDos[index].Description = Console.ReadLine();
-                    Console.Write("Priority (1-5): ");
-                    int.TryParse(Console.ReadLine(), out int priority);
-                    toDos[index].Priority = priority;
-                    Console.Write("Complete? (y/n): ");
-                    var complete = Console.ReadLine();
-                    bool done = false;
-                    if (complete?.ToLower() == "y")
-                        done = true;
-                    else
-                        done = false;
-                    toDos[index].IsCompleted = done;
-                    Console.WriteLine("ToDo updated.");
-                    return;
-                }
-            }
-        }
-
-        private static void ListAllToDos()
-        {
-            Console.WriteLine("All ToDos: ");
-            foreach (var t in toDos)
-                Console.WriteLine($"#{t.Id}: {t}");
-        }
-        private static bool DeleteProject()
-        {
-            Console.Write("Enter Project ID: ");
-            if (int.TryParse(Console.ReadLine(), out int id))
-            {
-                int index = -1;
-                for (int i = 0; i < projects.Count; i++)
-                    if (projects[i].Id == id)
-                    {
-                        index = i;
-                        break;
-                    }
-                if (index >= 0)
-                {
-                    projects.RemoveAt(index);
-                    return true;
-                }
-                else
-                {
-                    Console.WriteLine("Project ID invalid.");
-                    return false;
-                }
-            }
-            else
-            {
-                Console.WriteLine("Project ID invalid.");
-                return false;
-            }
-
-        }
-
-        private static void UpdateProject()
-        {
-            Console.Write("Enter Project ID: ");
-            if (int.TryParse(Console.ReadLine(), out int id))
-            {
-                int index = -1;
-                for (int i = 0; i < projects.Count; i++)
-                    if (projects[i].Id == id)
-                    {
-                        index = i;
-                        break;
-                    }
-                if (index >= 0)
-                {
-                    Console.Write("Project Name: ");
-                    projects[index].Name = Console.ReadLine();
-                    Console.Write("Description: ");
-                    projects[index].Description = Console.ReadLine();
-                    Console.Write("Add ToDo to Project (ToDo ID): ");
-                    if (int.TryParse(Console.ReadLine(), out int toDoId))
-                    {
-                        int toDoIndex = -1;
-                        for (int i = 0; i < toDos.Count; i++)
-                            if (toDos[i].Id == toDoId)
-                            {
-                                toDoIndex = i;
-                                break;
-                            }
-                        projects[index].ToDos.Add(toDos[toDoIndex]);
-                    }
-                    Console.Write("Completion Percentage: ");
-                    if (int.TryParse(Console.ReadLine(), out int percent))
-                        projects[index].CompletionPercent = percent;
-
-                }
-                else
-                    Console.WriteLine("Project ID not found.");
-            }
-        }
-        private static void ListAllProjects()
-        {
-            Console.WriteLine("All Projects:");
-            foreach (var p in projects)
-                Console.WriteLine($"#{p.Id}: {p}");
-        }
-
-        private static void ListGivenProjectToDo()
-        {
-            Console.Write("Enter Project ID: ");
-            if (!int.TryParse(Console.ReadLine(), out int id))
-            {
-                Console.WriteLine("Project ID invalid."); return;
-            }
-
-            Project? proj = null;
-            foreach (var p in projects)
-                if (p.Id == id)
-                {
-                    proj = p;
-                    break;
-                }
-            if (proj == null)
-            {
-                Console.WriteLine("Project ID invalid");
-                return;
-            }
-            Console.WriteLine($"ToDos in {proj.Name}:");
-            foreach (var t in proj.ToDos)
-            {
-                Console.WriteLine($"#{t.Id}: {t}");
-            }
-
-        }
-
-
-        private static ToDo CreateToDo()
-        {
-            Console.Write("Name: ");
-            var name = Console.ReadLine();
-            Console.Write("Description: ");
-            var description = Console.ReadLine();
-            Console.Write("Priority (1-5): ");
-            int.TryParse(Console.ReadLine(), out int priority);
-
-            return new ToDo
-            {
-                Id = nextToDoId++,
-                Name = name,
-                Description = description,
-                Priority = priority,
-                IsCompleted = false
-            };
-
-        }
-
-        private static Project CreateProject()
-        {
-            Console.Write("Project Name: ");
-            var name = Console.ReadLine();
-            Console.Write("Description: ");
-            var description = Console.ReadLine();
-
-            return new Project
-            {
-                Id = nextProjectId++,
-                Name = name,
-                Description = description,
-                CompletionPercent = 0,
-            };
-        }
+        
         
     }
 }
---
+++ Asana.Library/Services/ToDoServiceProxy.cs
@@ -0,0 +1,225 @@
+﻿using Asana.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asana.Library.Services
+{
+    public static class ToDoServiceProxy
+    {
+
+        public static bool DeleteToDo(List<ToDo> toDos)
+        {
+            Console.Write("Enter ToDo ID: ");
+            if (int.TryParse(Console.ReadLine(), out var id))
+            {
+                int index = -1;
+                for (int i = 0; i < toDos.Count; i++)
+                    if (toDos[i].Id == id)
+                    {
+                        index = i;
+                        break;
+                    }
+                if (index >= 0)
+                {
+                    toDos.RemoveAt(index);
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("ToDo ID invalid.");
+                    return false;
+                }
+            }
+            else
+            {
+                Console.WriteLine("ToDo ID invalid.");
+                return false;
+            }
+        }
+        public static void UpdateTodo(List<ToDo> toDos)
+        {
+            Console.Write("Enter ToDo ID: ");
+            if (int.TryParse(Console.ReadLine(), out var id))
+            {
+                int index = -1;
+                for (int i = 0; i < toDos.Count; i++)
+                    if (toDos[i].Id == id)
+                    {
+                        index = i;
+                        break;
+                    }
+                if (index >= 0)
+                {
+                    Console.Write("Name: ");
+                    toDos[index].Name = Console.ReadLine();
+                    Console.Write("Description: ");
+                    toDos[index].Description = Console.ReadLine();
+                    Console.Write("Priority (1-5): ");
+                    int.TryParse(Console.ReadLine(), out int priority);
+                    toDos[index].Priority = priority;
+                    Console.Write("Complete? (y/n): ");
+                    var complete = Console.ReadLine();
+                    bool done = false;
+                    if (complete?.ToLower() == "y")
+                        done = true;
+                    else
+                        done = false;
+                    toDos[index].IsCompleted = done;
+                    Console.WriteLine("ToDo updated.");
+                    return;
+                }
+            }
+        }
+
+        public static void ListAllToDos(List<ToDo> toDos)
+        {
+            Console.WriteLine("All ToDos: ");
+            foreach (var t in toDos)
+                Console.WriteLine($"#{t.Id}: {t}");
+        }
+        public static bool DeleteProject(List<Project> projects)
+        {
+            Console.Write("Enter Project ID: ");
+            if (int.TryParse(Console.ReadLine(), out int id))
+            {
+                int index = -1;
+                for (int i = 0; i < projects.Count; i++)
+                    if (projects[i].Id == id)
+                    {
+                        index = i;
+                        break;
+                    }
+                if (index >= 0)
+                {
+                    projects.RemoveAt(index);
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("Project ID invalid.");
+                    return false;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Project ID invalid.");
+                return false;
+            }
+
+        }
+
+        public static void UpdateProject(List<ToDo> toDos, List<Project> projects)
+        {
+            Console.Write("Enter Project ID: ");
+            if (int.TryParse(Console.ReadLine(), out int id))
+            {
+                int index = -1;
+                for (int i = 0; i < projects.Count; i++)
+                    if (projects[i].Id == id)
+                    {
+                        index = i;
+                        break;
+                    }
+                if (index >= 0)
+                {
+                    Console.Write("Project Name: ");
+                    projects[index].Name = Console.ReadLine();
+                    Console.Write("Description: ");
+                    projects[index].Description = Console.ReadLine();
+                    Console.Write("Add ToDo to Project (ToDo ID): ");
+                    if (int.TryParse(Console.ReadLine(), out int toDoId))
+                    {
+                        int toDoIndex = -1;
+                        for (int i = 0; i < toDos.Count; i++)
+                            if (toDos[i].Id == toDoId)
+                            {
+                                toDoIndex = i;
+                                break;
+                            }
+                        projects[index].ToDos.Add(toDos[toDoIndex]);
+                    }
+                    Console.Write("Completion Percentage: ");
+                    if (int.TryParse(Console.ReadLine(), out int percent))
+                        projects[index].CompletionPercent = percent;
+
+                }
+                else
+                    Console.WriteLine("Project ID not found.");
+            }
+        }
+        public static void ListAllProjects(List<Project> projects)
+        {
+            Console.WriteLine("All Projects:");
+            foreach (var p in projects)
+                Console.WriteLine($"#{p.Id}: {p}");
+        }
+
+        public static void ListGivenProjectToDo(List<Project> projects)
+        {
+            Console.Write("Enter Project ID: ");
+            if (!int.TryParse(Console.ReadLine(), out int id))
+            {
+                Console.WriteLine("Project ID invalid."); return;
+            }
+
+            Project? proj = null;
+            foreach (var p in projects)
+                if (p.Id == id)
+                {
+                    proj = p;
+                    break;
+                }
+            if (proj == null)
+            {
+                Console.WriteLine("Project ID invalid");
+                return;
+            }
+            Console.WriteLine($"ToDos in {proj.Name}:");
+            foreach (var t in proj.ToDos)
+            {
+                Console.WriteLine($"#{t.Id}: {t}");
+            }
+
+        }
+
+
+        public static ToDo CreateToDo(ref int nextToDoId)
+        {
+            Console.Write("Name: ");
+            var name = Console.ReadLine();
+            Console.Write("Description: ");
+            var description = Console.ReadLine();
+            Console.Write("Priority (1-5): ");
+            int.TryParse(Console.ReadLine(), out int priority);
+
+            return new ToDo
+            {
+                Id = nextToDoId++,
+                Name = name,
+                Description = description,
+                Priority = priority,
+                IsCompleted = false
+            };
+
+        }
+
+        public static Project CreateProject(ref int nextProjectId)
+        {
+            Console.Write("Project Name: ");
+            var name = Console.ReadLine();
+            Console.Write("Description: ");
+            var description = Console.ReadLine();
+
+            return new Project
+            {
+                Id = nextProjectId++,
+                Name = name,
+                Description = description,
+                CompletionPercent = 0,
+            };
+        }
+    }
+}
